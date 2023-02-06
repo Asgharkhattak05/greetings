e09ede7b3976613bd9524bc829599085f4e16dd6
Asghar Waheed <arhumkhattak05@gmail.com>
2023-02-06 17:10:05 +0500

    greeting file added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+
+let time=new Date().getHours();
+let greetings="";
+const cssStyle={ };
+
+if(time>=1 && time<=11){
+  greetings="Good Morning";
+  cssStyle.color="green";
+} 
+else if(time>=12 && time <=18){
+  greetings="Good AfterNone";
+  cssStyle.color="orange";
+}
+else{
+  greetings="good Night";
+  cssStyle.color="purple"
+}
+return <div><h1>Hello Sir, <span style={cssStyle}> {greetings}</span></h1></div>
 }
 
 export default App;
+
+
+
